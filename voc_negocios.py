1ed2946c63cdaf194c2ddc2906224d5c570ab325
Aurelio Ferrari <lelinhos@gmail.com>
2022-07-14 08:58:48 -0300

    subjuntivo

---
+++ voc_negocios.py
@@ -0,0 +1,313 @@
+# 1
+while True:
+    resp = str(input('Como digo -- Margem de Lucro--?\n')).strip().lower()
+    pont = 0
+    perg = 0
+    if resp == 'margen de ganancia':
+        pont += 1
+        perg += 1
+        print(f'MUY BIEN!! De momento, acertaste {pont} de {perg}.')
+    else:
+        perg += 1
+        print('La respuesta es: margen de ganancia.')
+        print(f'De momento, acertaste {pont} de {perg}.')
+
+    # 2
+    resp = str(input('Como digo -- Tom Correto--?\n')).strip().lower()
+    if resp == 'tono correcto' or resp == 'tono adecuado':
+        pont += 1
+        perg += 1
+        print(f'MUY BIEN!! De momento, acertaste {pont} de {perg}.')
+    else:
+        perg += 1
+        print('La respuesta es: tono correcto / adecuado.')
+        print(f'De momento, acertaste {pont} de {perg}.')
+
+    # 3
+    resp = str(input('Como digo -- Complexidade--?\n')).strip().lower()
+    if resp == 'complejidad':
+        pont += 1
+        perg += 1
+        print(f'MUY BIEN!! De momento, acertaste {pont} de {perg}.')
+    else:
+        perg += 1
+        print('La respuesta es: complejidad.')
+        print(f'De momento, acertaste {pont} de {perg}.')
+
+
+    # 4
+    resp = str(input('Como digo -- Sugestão--?\n')).strip().lower()
+    if resp == 'sugerencia':
+        pont += 1
+        perg += 1
+        print(f'MUY BIEN!! De momento, acertaste {pont} de {perg}.')
+    else:
+        perg += 1
+        print('La respuesta es: sugerencia.')
+        print(f'De momento, acertaste {pont} de {perg}.')
+
+
+    # 5
+    resp = str(input('Como digo -- Tomada de decisão--?\n')).strip().lower()
+    if resp == 'toma de decisión':
+        pont += 1
+        perg += 1
+        print(f'MUY BIEN!! De momento, acertaste {pont} de {perg}.')
+    else:
+        perg += 1
+        print('La respuesta es: toma de decisión.')
+        print(f'De momento, acertaste {pont} de {perg}.')
+
+
+    # 6
+    resp = str(input('Como digo -- Especialista --?\n')).strip().lower()
+    if resp == 'experto' or resp == 'esperta':
+        pont += 1
+        perg += 1
+        print(f'MUY BIEN!! De momento, acertaste {pont} de {perg}.')
+    else:
+        perg += 1
+        print('La respuesta es: experto / experta.')
+        print(f'De momento, acertaste {pont} de {perg}.')
+
+
+
+    # 7
+    resp = str(input('Como digo -- Recentemente --?\n')).strip().lower()
+    if resp == 'recientemente':
+        pont += 1
+        perg += 1
+        print(f'MUY BIEN!! De momento, acertaste {pont} de {perg}.')
+    else:
+        perg += 1
+        print('La respuesta es: recientemente.')
+        print(f'De momento, acertaste {pont} de {perg}.')
+
+
+
+    # 8
+    resp = str(input('Como digo -- Criativo --?\n')).strip().lower()
+    if resp == 'creativo' or resp == 'creativa':
+        pont += 1
+        perg += 1
+        print(f'MUY BIEN!! De momento, acertaste {pont} de {perg}.')
+    else:
+        perg += 1
+        print('La respuesta es: creativo / creativa.')
+        print(f'De momento, acertaste {pont} de {perg}.')
+
+
+    # 9
+    resp = str(input('Como digo -- Inovador --?\n')).strip().lower()
+    if resp == 'innovador' or resp == 'innovadora':
+        pont += 1
+        perg += 1
+        print(f'MUY BIEN!! De momento, acertaste {pont} de {perg}.')
+    else:
+        perg += 1
+        print('La respuesta es: innovador / innovadora.')
+        print(f'De momento, acertaste {pont} de {perg}.')
+
+
+    # 10
+    resp = str(input('Como digo -- metódico --?\n')).strip().lower()
+    if resp == 'metódico' or resp == 'metódica':
+        pont += 1
+        perg += 1
+        print(f'MUY BIEN!! De momento, acertaste {pont} de {perg}.')
+    else:
+        perg += 1
+        print('La respuesta es: metódico / metódica.')
+        print(f'De momento, acertaste {pont} de {perg}.')
+
+
+
+    # 11
+    resp = str(input('Como digo -- Rigoroso --?\n')).strip().lower()
+    if resp == 'riguroso' or resp == 'estricto' or resp == 'rigorosa' or resp == 'estricta':
+        pont += 1
+        perg += 1
+        print(f'MUY BIEN!! De momento, acertaste {pont} de {perg}.')
+    else:
+        perg += 1
+        print('La respuesta es: estricto o rigoroso.')
+        print(f'De momento, acertaste {pont} de {perg}.')
+
+
+
+    # 12
+    resp = str(input('Como digo -- Extrovertido --?\n')).strip().lower()
+    if resp == 'extravertido' or resp == 'extravertida':
+        pont += 1
+        perg += 1
+        print(f'MUY BIEN!! De momento, acertaste {pont} de {perg}.')
+    else:
+        perg += 1
+        print('La respuesta es: extravertido / extravertida.')
+        print(f'De momento, acertaste {pont} de {perg}.')
+
+
+    # 13
+    resp = str(input('Como digo -- Analisar --?\n')).strip().lower()
+    if resp == 'analizar':
+        pont += 1
+        perg += 1
+        print(f'MUY BIEN!! De momento, acertaste {pont} de {perg}.')
+    else:
+        perg += 1
+        print('La respuesta es: analizar - con Z.')
+        print(f'De momento, acertaste {pont} de {perg}.')
+
+
+    # 14
+    resp = str(input('Como digo -- Imprimir --?\n')).strip().lower()
+    if resp == 'imprimir':
+        pont += 1
+        perg += 1
+        print(f'MUY BIEN!! De momento, acertaste {pont} de {perg}.')
+    else:
+        perg += 1
+        print('La respuesta es: imprimir.')
+        print(f'De momento, acertaste {pont} de {perg}.')
+
+
+    # 15
+    resp = str(input('Como digo -- Contrato --?\n')).strip().lower()
+    if resp == 'contrato':
+        pont += 1
+        perg += 1
+        print(f'MUY BIEN!! De momento, acertaste {pont} de {perg}.')
+    else:
+        perg += 1
+        print('La respuesta es: contrato.')
+        print(f'De momento, acertaste {pont} de {perg}.')
+
+
+    # 16
+    resp = str(input('Como digo -- Impressora --?\n')).strip().lower()
+    if resp == 'impresora':
+        pont += 1
+        perg += 1
+        print(f'MUY BIEN!! De momento, acertaste {pont} de {perg}.')
+    else:
+        perg += 1
+        print('La respuesta es: impresora.')
+        print(f'De momento, acertaste {pont} de {perg}.')
+
+
+    # 17
+    resp = str(input('Como digo -- Apagar --?\n')).strip().lower()
+    if resp == 'borrar':
+        pont += 1
+        perg += 1
+        print(f'MUY BIEN!! De momento, acertaste {pont} de {perg}.')
+    else:
+        perg += 1
+        print('La respuesta es: borrar.')
+        print(f'De momento, acertaste {pont} de {perg}.')
+
+
+    # 18
+    resp = str(input('Como digo -- Pasta --?\n')).strip().lower()
+    if resp == 'carpeta':
+        pont += 1
+        perg += 1
+        print(f'MUY BIEN!! De momento, acertaste {pont} de {perg}.')
+    else:
+        perg += 1
+        print('La respuesta es: carpeta.')
+        print(f'De momento, acertaste {pont} de {perg}.')
+
+
+    # 19
+    resp = str(input('Como digo -- Grifado --?\n')).strip().lower()
+    if resp == 'resaltado':
+        pont += 1
+        perg += 1
+        print(f'MUY BIEN!! De momento, acertaste {pont} de {perg}.')
+    else:
+        perg += 1
+        print('La respuesta es: resaltado.')
+        print(f'De momento, acertaste {pont} de {perg}.')
+
+
+    # 20
+    resp = str(input('Como digo -- Preocupado com --?\n')).strip().lower()
+    if resp == 'preocupado por':
+        pont += 1
+        perg += 1
+        print(f'MUY BIEN!! De momento, acertaste {pont} de {perg}.')
+    else:
+        perg += 1
+        print('La respuesta es: preocupado por.')
+        print(f'De momento, acertaste {pont} de {perg}.')
+
+
+
+    # 21
+    resp = str(input('Como digo -- Se demitir --?\n')).strip().lower()
+    if resp == 'dimitir' or resp == 'renunciar':
+        pont += 1
+        perg += 1
+        print(f'MUY BIEN!! De momento, acertaste {pont} de {perg}.')
+    else:
+        perg += 1
+        print('La respuesta es: dimitir / renunciar.')
+        print(f'De momento, acertaste {pont} de {perg}.')
+
+
+    # 22
+    resp = str(input('Como digo -- Licença maternidade --?\n')).strip().lower()
+    if resp == 'licencia de maternidad' or resp == 'baja por maternidad':
+        pont += 1
+        perg += 1
+        print(f'MUY BIEN!! De momento, acertaste {pont} de {perg}.')
+    else:
+        perg += 1
+        print('La respuesta es: licencia DE maternidad / baja POR maternidad.')
+        print(f'De momento, acertaste {pont} de {perg}.')
+
+
+    # 23
+    resp = str(input('Como digo -- terceirizar --?\n')).strip().lower()
+    if resp == 'subcontratar':
+        pont += 1
+        perg += 1
+        print(f'MUY BIEN!! De momento, acertaste {pont} de {perg}.')
+    else:
+        perg += 1
+        print('La respuesta es: subcontratar.')
+        print(f'De momento, acertaste {pont} de {perg}.')
+
+
+
+    # 24
+    resp = str(input('Como digo -- terceirizada --?\n')).strip().lower()
+    if resp == 'subcontrata':
+        pont += 1
+        perg += 1
+        print(f'MUY BIEN!! De momento, acertaste {pont} de {perg}.')
+    else:
+        perg += 1
+        print('La respuesta es: subcontrata.')
+        print(f'De momento, acertaste {pont} de {perg}.')
+
+
+
+    # 25
+    resp = str(input('Como digo -- adiar --?\n')).strip().lower()
+    if resp == 'aplazar':
+        pont += 1
+        perg += 1
+        print(f'MUY BIEN!! De momento, acertaste {pont} de {perg}.')
+    else:
+        perg += 1
+        print('La respuesta es: aplazar.')
+        print(f'De momento, acertaste {pont} de {perg}.')
+
+    if pont == 25:
+        print('Felicitaciones. Te salió todo perfecto.')
+        break
+    if pont < 25:
+        print(f'Que lástima. Solo acertaste {pont} de {perg}.\nEmpiézalo de nuevo.\n')
+salir = str(input('Digite qualquer tecla para sair: '))
